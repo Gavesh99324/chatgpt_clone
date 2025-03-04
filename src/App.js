ebac49e97edfca48c4ef0cae62850cda4fa48fdf
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-03-05 01:52:41 +0530

    Create the Main Interface of chatGPT Clone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
 
 function App() {
   return (
-    <div>
-     
+    <div className='App'>
+      <div className='sidebar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'><img className='logo' src={gptLogo} alt='' /><span className='brand'>ChatGPT</span></div>
+          <button className='midBtn'><img className='addBtn' src='' alt='' />New Chat</button>
+          <div className='upperSideBottom'>
+            <button className='query'><img className='' src='' alt='' />What is Programming ?</button>
+          </div>
+        </div>
+        <div className='lowerSide'>
+  
+        </div>
+      </div>
+
+
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
